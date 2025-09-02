55f38942c04fe3efa05d41a4a7f2b17763228d1f
Leozera2k <leofsousa98@gmail.com>
2025-09-02 14:57:15 -0300

    criando rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+
 function App() {
   return (
-    <div className="App">
-     Olá Mundo
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
-      <button>Fazer Login</button>
+      <Link to="/login">Fazer Login</Link>
     </>
   );
 };
+
+export { Home };
