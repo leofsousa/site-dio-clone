cb6e7967655f7b56095e5ed0b2f1c21b7e3abc38
Leozera2k <leofsousa98@gmail.com>
2025-09-04 15:21:23 -0300

    tela de login criada

---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+export const InputContainer = styled.div`
+    max-width: 275px;
+    width: 100%;
+    height: 30px;
+    border: 1px solid #3b3450;
+
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`
+export const IconContainer = styled.div`
+    margin: 0 10px;
+    
+`
+
+export const InputText = styled.input`
+    background-color: transparent;
+    color: #FFFFFF;
+    width: 100%;
+    border: 0;
+    height: 30px;
+`
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,12 +1,48 @@
-import { Link } from "react-router-dom"
+import { MdEmail, MdLock } from "react-icons/md";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import {
+  Container,
+  Column,
+  CriarText,
+  EsqueciText,
+  Row,
+  Title,
+  TitleLogin,
+  Wrapper,
+  SubtitleLogin,
+} from "./styles";
+import { Input } from "../../components/Input";
 
 const Login = () => {
   return (
     <>
-      <h1>Login</h1>
-      <Link to='/'>Voltar para home</Link>
+      <Header />
+      <Container>
+        <Column>
+          <Title>
+            A Plataforma para você aprender com experts, dominar as principais
+            tecnologias e entrar mais rápido nas empresas mais desejadas.
+          </Title>
+        </Column>
+        <Column>
+          <Wrapper>
+            <TitleLogin>Faça seu Cadastro</TitleLogin>
+            <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+            <form>
+              <Input placeholder="Email" leftIcon={<MdEmail/>} />
+              <Input placeholder="Senha" type="password" leftIcon={<MdLock />}/>
+              <Button title="Entrar" variant="secondary"></Button>
+            </form>
+            <Row>
+              <EsqueciText>Esqueci minha senha</EsqueciText>
+              <CriarText>Criar Conta</CriarText>
+            </Row>
+          </Wrapper>
+        </Column>
+      </Container>
     </>
   );
 };
 
-export { Login }; 
+export { Login };
